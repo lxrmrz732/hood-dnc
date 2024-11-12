ca43ca138c030e998784cdc450167169f3bb90d3
Alex Ramírez on 6470beast <lxrmrz732@gmail.com>
2024-11-12 16:05:26 -0500

    serial_configure initial shitty implementation

--- src/serial/serial.c
+++ src/serial/serial.c
@@ -2,76 +2,75 @@
  * Serial interface code to make hood_dnc more simple.
  * This uses an RS-232 serial port on your system!! (please be in the "dialout" group)
  *
- * Input uses "Canonical Input Processing" because NC code always begins with '%' and ends with '%'.
- * Output is undecided because I have no idea.
- *
  * Sources:
  * https://tldp.org/HOWTO/Serial-Programming-HOWTO/
  * https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
  */
+#include "serial.h"
 #include <alloca.h>
-#include <asm-generic/termbits-common.h>  // needed to c89
+#include <asm-generic/termbits-common.h> // needed to c89
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-#include "serial.h"
 
 int serial_open(char *path_to_port) {
-    /* symbols */
-    int fd = -1;
+	/* symbols */
+	int fd = -1;
 
-    /* straight from source #1 */
-    fd = open(path_to_port, O_RDWR | O_NOCTTY);
+	/* straight from source #1 */
+	fd = open(path_to_port, O_RDWR | O_NOCTTY);
 
-    /* error check file descriptor */
-    if (fd < 0) {
-        perror(path_to_port);
-    }
+	/* error check file descriptor */
+	if (fd < 0) {
+		perror(path_to_port);
+	}
 
-    /* yay */
-    return fd;
+	/* yay */
+	return fd;
 }
 
 int serial_configure(int fd, speed_t rate, struct termios *old_config) {
-    /* straight from source #2 */
-    struct termios new_config = {0};
+	/* straight from source #2 */
+	struct termios new_config = {0};
 
-    /* acquire and backup the current attributes*/
-    if (tcgetattr(fd, &new_config) < 0) {
-        (void)printf("Error from tcgetattr: %s\n", strerror(errno));
-        return -1;
-    }
-    (void)memcpy(old_config, &new_config, sizeof(struct termios));
+	/* acquire and backup the current attributes*/
+	if (tcgetattr(fd, &new_config) < 0) {
+		(void)printf("Error from tcgetattr: %s\n", strerror(errno));
+		return -1;
+	}
+	(void)memcpy(old_config, &new_config, sizeof(struct termios));
 
-    /* set baud rate */
-    if (cfsetospeed(&new_config, (speed_t)rate) || cfsetispeed(&new_config, (speed_t)rate)) {
-        (void)printf("Error setting baud rate: %s\n", strerror(errno));
-        return -1;
-    }
+	/* set baud rate */
+	if (cfsetospeed(&new_config, (speed_t)rate) || cfsetispeed(&new_config, (speed_t)rate)) {
+		(void)printf("Error setting baud rate %d: %s\n", rate, strerror(errno));
+		return -1;
+	}
 
-    /* weirdo settings */
-    new_config.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
-    new_config.c_cflag &= ~CSIZE;
-    new_config.c_cflag |= CS8; /* 8-bit characters */
-    // new_config.c_cflag &= ~PARENB;  /* no parity bit */ // should turn on instead
-    new_config.c_cflag &= ~CSTOPB;  /* only need 1 stop bit */
-    new_config.c_cflag &= ~CRTSCTS; /* no hardware flowcontrol */
+	/* weirdo settings */
+	new_config.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
+	new_config.c_cflag &= ~CSIZE;			/* invert CSIZE to later set CS8 */
+	new_config.c_cflag |= CS8;				/* 8-bit characters */
+	new_config.c_cflag &= ~PARENB;			/* no parity bit; should turn on */
+	new_config.c_cflag &= ~CSTOPB;			/* only need 1 stop bit */
+	new_config.c_cflag &= ~CRTSCTS;			/* no hardware flowcontrol */
 
-    /* setup for non-canonical mode */
-    new_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-    new_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-    new_config.c_oflag &= ~OPOST;
+	/* setup for non-canonical mode */
+	new_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	new_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	new_config.c_oflag &= ~OPOST;
 
-    /* fetch bytes as they become available */
-    new_config.c_cc[VMIN] = 1;
-    new_config.c_cc[VTIME] = 1;
+	/* fetch bytes as they become available */
+	new_config.c_cc[VMIN] = 1;
+	new_config.c_cc[VTIME] = 1;
 
-    if (tcsetattr(fd, TCSANOW, &new_config) != 0) {
-        printf("Error from tcsetattr: %s\n", strerror(errno));
-        return -1;
-    }
-    return 0;
+	/* actually set the attributes */
+	if (tcsetattr(fd, TCSANOW, &new_config) != 0) {
+		printf("Error from tcsetattr: %s\n", strerror(errno));
+		return -1;
+	}
+	return 0;
 }
+
--- src/serial/serial.h
+++ src/serial/serial.h
@@ -12,7 +12,17 @@
  */
 int serial_open(char *path_to_port);
 
-// struct termios *serial_configure(int unknown);
+/**
+ * Configure the serial port for use with Hood DNC.
+ * Backup the old configuration and set a hardcoded new configuration.
+ *
+ * @param fd file descriptor to open serial port
+ * @param rate desired baud rate of serial port
+ * @param old_config pointer to a space to backup old configuration
+ *
+ * @return 0 if the port was configured successfully, -1 otherwise
+ */
+int serial_configure(int fd, speed_t rate, struct termios *old_config);
 
 /**
  * Close the serial port in use with Hood DNC.
