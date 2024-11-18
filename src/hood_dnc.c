bd2fb33b58f30417e7f79cccb0dffb6f8bcd797d
Alex Ramírez on precision-5540 <lxrmrz732@gmail.com>
2024-11-18 16:10:32 -0500

    rolling eyes

--- src/hood_dnc.c
+++ src/hood_dnc.c
@@ -29,69 +29,69 @@ int nc_receive(int serial_fd, char *file_path);
  * chad comment
  */
 int main(int argc, char *argv[]) {
-    /* freaky symbols */
-    struct termios backup_config = {0};
-    char *serial_path = NULL;
-    char *file_path = NULL;
-    speed_t baud_rate = -1; /* try (15 = B38400), (4098 = B115200), or (13 = B9600) for mill */
-    int serial_fd = -1;
-    int helper_return = -1;
-    char mode = -1;
+	/* freaky symbols */
+	struct termios backup_config = {0};
+	char *serial_path = NULL;
+	char *file_path = NULL;
+	speed_t baud_rate = -1; /* try (15 = B38400), (4098 = B115200), or (13 = B9600) for mill */
+	int serial_fd = -1;
+	int helper_return = -1;
+	char mode = -1;
 
-    /* check the params; this is a "no stdin" household */
-    if (argc != 5) {
-        (void)fprintf(stderr, "Usage: %s [baud rate] [serial port] [file to send/receive] ['s'end/'r'eceive]\n", PROG);
-        return 1;
-    }
+	/* check the params; this is a "no stdin" household */
+	if (argc != 5) {
+		(void)fprintf(stderr, "Usage: %s [baud rate] [serial port] [file to send/receive] ['s'end/'r'eceive]\n", PROG);
+		return 1;
+	}
 
-    /* grab the params */
-    baud_rate = (speed_t)atoi(argv[1]);
-    serial_path = argv[2];
-    file_path = argv[3];
-    mode = argv[4][0];
+	/* grab the params */
+	baud_rate = (speed_t)atoi(argv[1]);
+	serial_path = argv[2];
+	file_path = argv[3];
+	mode = argv[4][0];
 
-    /* sanitize params */
-    if (baud_rate == 0) {
-        ERROR("atoi failed; try a real number for baud rate");
-        return 2;
-    }
-    if (mode != SEND && mode != RECEIVE) {
-        ERROR("file mode MUST be 's'end or 'r'eceive");
-        return 3;
-    }
+	/* sanitize params */
+	if (baud_rate == 0) {
+		ERROR("atoi failed; try a real number for baud rate");
+		return 2;
+	}
+	if (mode != SEND && mode != RECEIVE) {
+		ERROR("file mode MUST be 's'end or 'r'eceive");
+		return 3;
+	}
 
-    /* open and configure the serial port */
-    serial_fd = serial_open(serial_path);
-    if (serial_fd == -1) {
-        ERROR("failed to open the mf serial port");
-        return 4;
-    }
-    if ((helper_return = serial_configure(serial_fd, baud_rate, &backup_config))) {
-        ERROR("failed to configure the mf serial port");
-        mode = FAIL;
-    }
+	/* open and configure the serial port */
+	serial_fd = serial_open(serial_path);
+	if (serial_fd == -1) {
+		ERROR("failed to open the mf serial port");
+		return 4;
+	}
+	if ((helper_return = serial_configure(serial_fd, baud_rate, &backup_config))) {
+		ERROR("failed to configure the mf serial port");
+		mode = FAIL;
+	}
 
-    /* yay branching */
-    switch (mode) {
-        case SEND: {
-            helper_return = nc_send(serial_fd, file_path);
-            break;
-        }
-        case RECEIVE: {
-            helper_return = nc_receive(serial_fd, file_path);
-            break;
-        }
-        default:
-            ERROR("program control flow failed; exiting");
-            break;
-    }
+	/* yay branching */
+	switch (mode) {
+	case SEND: {
+		helper_return = nc_send(serial_fd, file_path);
+		break;
+	}
+	case RECEIVE: {
+		helper_return = nc_receive(serial_fd, file_path);
+		break;
+	}
+	default:
+		ERROR("program control flow failed; exiting");
+		break;
+	}
 
-    /* close the serial port */
-    if (serial_close(serial_fd, &backup_config)) {
-        ERROR("failed to close the mf serial port");
-        return 7;
-    }
-    return helper_return;
+	/* close the serial port */
+	if (serial_close(serial_fd, &backup_config)) {
+		ERROR("failed to close the mf serial port");
+		return 7;
+	}
+	return helper_return;
 }
 
 /**
@@ -103,43 +103,43 @@ int main(int argc, char *argv[]) {
  * @return zero on success, nonzero if anything went wrong
  */
 int nc_send(int serial_fd, char *file_path) {
-    /* symbols */
-    FILE *nc_file = NULL;
-    char *data_buffer = NULL;
-    size_t bytes_read = 0;
-    int status = 0;
+	/* symbols */
+	FILE *nc_file = NULL;
+	char *data_buffer = NULL;
+	size_t bytes_read = 0;
+	int status = 0;
 
-    /* allocate space */
-    data_buffer = alloca(sizeof(char) * BUFSIZ);
+	/* allocate space */
+	data_buffer = alloca(sizeof(char) * BUFSIZ);
 
-    /* open and read from the file */
-    nc_file = fopen(file_path, "r");
-    if (nc_file == NULL) {
-        IO_ERROR("error opening NC file for reading", file_path, errno);
-        return 10;
-    }
+	/* open and read from the file */
+	nc_file = fopen(file_path, "r");
+	if (nc_file == NULL) {
+		IO_ERROR("error opening NC file for reading", file_path, errno);
+		return 10;
+	}
 
-    /* write contents to the serial port as they are read from the file */
-    while ((bytes_read = fread(data_buffer, sizeof(char), BUFSIZ, nc_file))) {
-        if (write(serial_fd, data_buffer, bytes_read) == -1) {
-            IO_ERROR("error writing", "serial port", errno);
-            status = 11;
-            break;
-        }
-    }
+	/* write contents to the serial port as they are read from the file */
+	while ((bytes_read = fread(data_buffer, sizeof(char), BUFSIZ, nc_file))) {
+		if (write(serial_fd, data_buffer, bytes_read) == -1) {
+			IO_ERROR("error writing", "serial port", errno);
+			status = 11;
+			break;
+		}
+	}
 
-    /* error "handling" */
-    if (ferror(nc_file)) {
-        IO_ERROR("error reading NC file", file_path, errno);
-        status = 12;
-    }
+	/* error "handling" */
+	if (ferror(nc_file)) {
+		IO_ERROR("error reading NC file", file_path, errno);
+		status = 12;
+	}
 
-    /* TODO: send end-of-transmission data so the machine finishes */
-    if (fclose(nc_file)) {
-        IO_ERROR("error closing NC file", file_path, errno);
-        status = 13;
-    }
-    return status;
+	/* TODO: send end-of-transmission data so the machine finishes */
+	if (fclose(nc_file)) {
+		IO_ERROR("error closing NC file", file_path, errno);
+		status = 13;
+	}
+	return status;
 }
 
 /**
@@ -151,51 +151,51 @@ int nc_send(int serial_fd, char *file_path) {
  * @return zero on success, nonzero if anything went wrong
  */
 int nc_receive(int serial_fd, char *file_path) {
-    /* symbols 2.0 */
-    FILE *nc_file = NULL;
-    char *data_buffer = NULL;
-    ssize_t bytes_read = 0;
-    size_t bytes_written = 0;
-    int status = 0;
+	/* symbols 2.0 */
+	FILE *nc_file = NULL;
+	char *data_buffer = NULL;
+	ssize_t bytes_read = 0;
+	size_t bytes_written = 0;
+	int status = 0;
 
-    /* allocate space; BUFSIZ is too much, it seems */
-    data_buffer = alloca(sizeof(char) * MAX_SERIAL_READ);
+	/* allocate space; BUFSIZ is too much, it seems */
+	data_buffer = alloca(sizeof(char) * MAX_SERIAL_READ);
 
-    /* open file for writing */
-    nc_file = fopen(file_path, "w");
-    if (nc_file == NULL) {
-        IO_ERROR("error opening NC file for writing", file_path, errno);
-        return 20;
-    }
+	/* open file for writing */
+	nc_file = fopen(file_path, "w");
+	if (nc_file == NULL) {
+		IO_ERROR("error opening NC file for writing", file_path, errno);
+		return 20;
+	}
 
-    /* this is "read with timeout" according to man termios */
-    while ((bytes_read = read(serial_fd, data_buffer, MAX_SERIAL_READ))) {
-        if (bytes_read == -1) {
-            IO_ERROR("error reading", "serial port", errno);
-            status = 21;
-            break;
-        }
-        if (!(bytes_written = fwrite(data_buffer, sizeof(char), bytes_read, nc_file))) {
-            if (ferror(nc_file)) {
-                IO_ERROR("error writing", file_path, errno);
-                status = 22;
-                break;
-            } else {
-                ERROR("successfully wrote zero bytes?");
-                status = 23;
-                break;
-            }
-        }
-    }
+	/* this is "read with timeout" according to man termios */
+	while ((bytes_read = read(serial_fd, data_buffer, MAX_SERIAL_READ))) {
+		if (bytes_read == -1) {
+			IO_ERROR("error reading", "serial port", errno);
+			status = 21;
+			break;
+		}
+		if (!(bytes_written = fwrite(data_buffer, sizeof(char), bytes_read, nc_file))) {
+			if (ferror(nc_file)) {
+				IO_ERROR("error writing", file_path, errno);
+				status = 22;
+				break;
+			} else {
+				ERROR("successfully wrote zero bytes?");
+				status = 23;
+				break;
+			}
+		}
+	}
 
-    /* flush contents; close files */
-    if (fflush(nc_file)) {
-        IO_ERROR("error flushing", file_path, errno);
-        status = 24;
-    }
-    if (fclose(nc_file)) {
-        IO_ERROR("error closing NC file", file_path, errno);
-        status = 25;
-    }
-    return status;
+	/* flush contents; close files */
+	if (fflush(nc_file)) {
+		IO_ERROR("error flushing", file_path, errno);
+		status = 24;
+	}
+	if (fclose(nc_file)) {
+		IO_ERROR("error closing NC file", file_path, errno);
+		status = 25;
+	}
+	return status;
 }
--- src/serial/serial.c
+++ src/serial/serial.c
@@ -18,79 +18,79 @@
 #include <unistd.h>
 
 int serial_open(char *path_to_port) {
-    /* symbols */
-    int fd = -1;
+	/* symbols */
+	int fd = -1;
 
-    /* straight from source #1 */
-    fd = open(path_to_port, O_RDWR | O_NOCTTY);
+	/* straight from source #1 */
+	fd = open(path_to_port, O_RDWR | O_NOCTTY);
 
-    /* error check file descriptor */
-    if (fd < 0) {
-        IO_ERROR("serial port error", path_to_port, errno);
-    }
+	/* error check file descriptor */
+	if (fd < 0) {
+		IO_ERROR("serial port error", path_to_port, errno);
+	}
 
-    /* yay */
-    return fd;
+	/* yay */
+	return fd;
 }
 
 int serial_configure(int fd, speed_t rate, struct termios *old_config) {
-    /* straight from source #2 */
-    struct termios new_config = {0};
+	/* straight from source #2 */
+	struct termios new_config = {0};
 
-    /* acquire and backup the current attributes*/
-    if (tcgetattr(fd, &new_config) < 0) {
-        IO_ERROR("opening error", "tcsetattr", errno)
-        return -1;
-    }
-    (void)memcpy(old_config, &new_config, sizeof(struct termios));
+	/* acquire and backup the current attributes*/
+	if (tcgetattr(fd, &new_config) < 0) {
+		IO_ERROR("opening error", "tcsetattr", errno)
+		return -1;
+	}
+	(void)memcpy(old_config, &new_config, sizeof(struct termios));
 
-    /* set baud rate */
-    if (cfsetospeed(&new_config, (speed_t)rate) || cfsetispeed(&new_config, (speed_t)rate)) {
-        IO_ERROR("error setting baud rate", "cfsetiospeed", errno)
-        return -1;
-    }
+	/* set baud rate */
+	if (cfsetospeed(&new_config, (speed_t)rate) || cfsetispeed(&new_config, (speed_t)rate)) {
+		IO_ERROR("error setting baud rate", "cfsetiospeed", errno)
+		return -1;
+	}
 
-    /* weirdo settings taken verbatim from source #2 */
-    new_config.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
-    new_config.c_cflag &= ~CSIZE;           /* invert CSIZE to later set CS8 */
-    new_config.c_cflag |= CS8;              /* 8-bit characters */
-    new_config.c_cflag &= ~PARENB;          /* no parity bit */
-    new_config.c_cflag &= ~CSTOPB;          /* only need 1 stop bit */
-    new_config.c_cflag &= ~CRTSCTS;         /* no hardware flowcontrol */
+	/* weirdo settings taken verbatim from source #2 */
+	new_config.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
+	new_config.c_cflag &= ~CSIZE;			/* invert CSIZE to later set CS8 */
+	new_config.c_cflag |= CS8;				/* 8-bit characters */
+	new_config.c_cflag &= ~PARENB;			/* no parity bit */
+	new_config.c_cflag &= ~CSTOPB;			/* only need 1 stop bit */
+	new_config.c_cflag &= ~CRTSCTS;			/* no hardware flowcontrol */
 
-    /* setup for non-canonical mode */
-    new_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
-    new_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-    new_config.c_oflag &= ~OPOST;
+	/* setup for non-canonical mode */
+	new_config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+	new_config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+	new_config.c_oflag &= ~OPOST;
 
-    /* setup for timed read */
-    new_config.c_cc[VMIN] = 0;
-    new_config.c_cc[VTIME] = READ_TIMEOUT_SEC;
+	/* setup for timed read */
+	new_config.c_cc[VMIN] = 0;
+	new_config.c_cc[VTIME] = READ_TIMEOUT_SEC;
 
-    /* break the hivemind; set even parity and hardware flow control */
-    new_config.c_cflag |= CRTSCTS;
-    new_config.c_cflag |= PARENB;
+	/* break the hivemind; set even parity and hardware flow control */
+	new_config.c_cflag |= CRTSCTS;
+	new_config.c_cflag |= PARENB;
 
-    /* actually set the attributes */
-    if (tcsetattr(fd, TCSANOW, &new_config) != 0) {
-        IO_ERROR("configuration error", "tcsetattr", errno)
-        return -1;
-    }
-    return 0;
+	/* actually set the attributes */
+	if (tcsetattr(fd, TCSANOW, &new_config) != 0) {
+		IO_ERROR("configuration error", "tcsetattr", errno)
+		return -1;
+	}
+	return 0;
 }
 
 int serial_close(int fd, struct termios *old_config) {
-    /* fr don't do it */
-    if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
-        ERROR("nah bruh don't close those");
-        return -1;
-    }
+	/* fr don't do it */
+	if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
+		ERROR("nah bruh don't close those");
+		return -1;
+	}
 
-    /* set the old parameters bro; idgaf if it fails */
-    if (old_config && tcsetattr(fd, TCSANOW, old_config)) {
-        IO_ERROR("closing error", "tcsetattr", errno)
-    }
+	/* set the old parameters bro; idgaf if it fails */
+	if (old_config && tcsetattr(fd, TCSANOW, old_config)) {
+		IO_ERROR("closing error", "tcsetattr", errno)
+	}
 
-    /* close the mf port */
-    return close(fd);
+	/* close the mf port */
+	return close(fd);
 }
