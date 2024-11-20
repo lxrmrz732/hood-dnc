e46e4b67a763b4f100e307aa12af4324920faccb
Alex Ramírez on 6470beast <lxrmrz732@gmail.com>
2024-11-20 16:40:51 -0500

    added port flags document to confirm that mine are the same as cadem ncnetlite 7

--- src/serial/serial.c
+++ src/serial/serial.c
@@ -2,18 +2,6 @@
  * Serial interface code to make hood_dnc more simple.
  * This uses an RS-232 serial port on your system!! (please be in the "dialout" group)
  *
- * stty -a -F /dev/ttyUSB0
- * speed 38400 baud; rows 0; columns 0; line = 0;
- * intr = ^C; quit = ^\; erase = ^?; kill = ^U; eof = <undef>; eol = <undef>; eol2 = <undef>;
- * swtch = <undef>; start = ^Q; stop = ^S; susp = ^Z; rprnt = ^R; werase = ^W; lnext = ^V; discard = ^O;
- * min = 0; time = 0;
- * parenb -parodd -cmspar cs8 -hupcl -cstopb cread clocal -crtscts
- * ignbrk -brkint -ignpar -parmrk inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel
- * -iutf8
- * -opost -olcuc -ocrnl onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0
- * -isig -icanon -iexten -echo echoe echok -echonl noflsh -xcase -tostop -echoprt echoctl echoke -flusho
- * -extproc
- *
  * Sources:
  * https://tldp.org/HOWTO/Serial-Programming-HOWTO/
  * https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
